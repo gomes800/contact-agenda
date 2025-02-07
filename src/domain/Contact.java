286489c19a25591983f368830a0e857274ecd682
barox <joaogomesleite800@gmail.com>
2025-02-07 13:50:40 -0300

    Class Contact

---
+++ src/domain/Contact.java
@@ -0,0 +1,41 @@
+package domain;
+
+public class Contact {
+
+    private Long id;
+    private String name;
+    private Long phone;
+    private String email;
+
+    public Contact() {}
+
+    public Contact(String name, Long phone, String email) {
+        this.name = name;
+        this.phone = phone;
+        this.email = email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Long getPhone() {
+        return phone;
+    }
+
+    public void setPhone(Long phone) {
+        this.phone = phone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
