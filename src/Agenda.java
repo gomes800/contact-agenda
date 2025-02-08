2be055ac68e5ebc8e643f791ea7ed67e68611654
barox <joaogomesleite800@gmail.com>
2025-02-08 11:53:13 -0300

    Class Agenda

--- src/Agenda.java
+++ src/Agenda.java
@@ -1,5 +1,9 @@
+import controllers.ContactManager;
+
 public class Agenda {
     public static void main(String[] args) {
+        ContactManager contactManager = new ContactManager();
 
+        contactManager.initializer();
     }
 }
